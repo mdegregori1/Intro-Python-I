8bd25f518c68b8fb32ce05a0f2c7aa717dceb4f0
Mauricio Degregori <degregori2@gmail.com>
2020-04-06 16:22:35 -0500

    01-03 completed

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,9 +14,11 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+z = x + int(y)
+print(z)
 
 # Write a print statement that combines x + y into the string value 57
-
+z = str(x) + y
+print(z)
 
 # YOUR CODE HERE
